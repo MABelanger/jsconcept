cfb7aec80707f59531f4abc90a6c3e8c2823642c
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2017-01-21 03:01:18 -0500

    update

--- es6/src/index.js
+++ es6/src/index.js
@@ -1,7 +1,12 @@
-class Task {
-  constructor() {
-    console.log('constructing Task');
+class Project {
+  constructor(name) {
+    this.location = 'Maztlan';
   }
-};
-let task = {};
-Task.call(task);
+}
+class SoftwareProject extends Project {
+  constructor() {
+    super();
+  }
+}
+let p = new SoftwareProject();
+console.log(p.location);
